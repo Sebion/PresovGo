aadf93df6a00e268c43975e4f304b8c8a1263a50
Sebastian Hresko <hresko@spse-po.sk>
2019-03-19 08:39:39 +0100

    opravene Mirove

--- app/src/main/java/com/example/sebastian/presovgo2/SakralneObjekty.java
+++ app/src/main/java/com/example/sebastian/presovgo2/SakralneObjekty.java
@@ -2,29 +2,37 @@ package com.example.sebastian.presovgo2;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class FetchData extends AsyncTask<Void,Void,Void> {
+public class SakralneObjekty extends AsyncTask<Void,Void,Void> {
+
     String data;
 
     @Override
-    protected void doInBakcground(Void... voids) {
-       try {
-           URL url = new URL("https://egov.presov.sk/GeoDataKatalog/sakralne_objekty.json");
-           HttpURLConnection httpURLConnection = url.openConnection();
-           InputStream inputStream = httpURLConnection.getInputStream();
-           BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-           String line = "";
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://egov.presov.sk/GeoDataKatalog/fontany_pramene.json");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
 
-           while(line != null) {
-               line = bufferedReader.readLine();
-               data = data + line;
-           }
+            while(line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
 
-       } catch (MalFormedURLException e) {
-           e.printStackTrace();
-       } catch (IOException e){
-           e.printStackTrace();
-       }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
         return null;
     }
@@ -34,5 +42,5 @@ public class FetchData extends AsyncTask<Void,Void,Void> {
         super.onPostExecute(aVoid);
     }
 
-}
 
+}
