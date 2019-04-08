5251d017f1a02f2d5a17361dd3fe1af6dd72b676
Sebastian Hresko <hreskosebastian@gmail.com>
2019-04-08 22:20:39 +0200

    markery na kostoly

--- app/src/main/java/com/example/sebastian/presovgo2/MapFragment.java
+++ app/src/main/java/com/example/sebastian/presovgo2/MapFragment.java
@@ -1,7 +1,9 @@
 package com.example.sebastian.presovgo2;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -15,6 +17,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 
 /**
@@ -45,15 +49,38 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         return v;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng presov= new LatLng(48.997631, 21.2401873);
-        mMap.addMarker(new MarkerOptions().position(presov).title("Marker in Presov"));
+        ArrayList <LatLng> markers=new ArrayList<>();
+        // Add a marker in Sydney and move the camera
+
+        markers.add(new LatLng(48.998058, 21.239890));
+        markers.add(new LatLng(48.998492, 21.239862));
+        markers.add(new LatLng(48.994157, 21.242976));
+        markers.add(new LatLng(48.989799, 21.244289));
+        markers.add(new LatLng(49.000021, 21.237849));
+        markers.add(new LatLng(48.989448, 21.227343));
+        markers.add(new LatLng(48.988057, 21.225881));
+        markers.add(new LatLng(48.979994, 21.276552));
+        markers.add(new LatLng(48.996245, 21.243839));
+        markers.add(new LatLng(48.998492, 21.239862));
+        markers.add(new LatLng(49.006799, 21.225588));
+        markers.add(new LatLng(48.994075, 21.272423));
+
+        markers.add(new LatLng(49.002234, 21.232797));
+        markers.add(new LatLng(48.978732, 21.269154));
+        markers.add(new LatLng(48.983558, 21.175571));
+        markers.add(new LatLng(48.996943, 21.286334));
+        markers.add(new LatLng(49.020747, 21.281516));
+
+        markers.forEach((n) ->  mMap.addMarker(new MarkerOptions().position(n)));
+
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(presov));
-        mMap.setMinZoomPreference(12);
+        mMap.setMinZoomPreference(11);
 
     }
 }
